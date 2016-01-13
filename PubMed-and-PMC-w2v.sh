b5911dc049b27e1aad8f743164703f8c45d846cc
Jake Marcus <jakerochlinmarcus@gmail.com>
2016-01-12 18:31:00 -0800

    minor change

--- PubMed-and-PMC-w2v.sh
+++ PubMed-and-PMC-w2v.sh
@@ -1,5 +1,5 @@
-python get_vocab.py PubMed-and-PMC-w2v.bin vocab.csv
-umls.db < umls.sql # merge vocab.csv with the UMLS database
+python get_vocab.py PubMed-and-PMC-w2v.bin vocab.csv # assumes you have downloaded the word embeddings
+umls.db < umls.sql # merge vocab.csv with the UMLS database (assumes you have installed the UMLS database)
 python reduce_vocab.py PubMed-and-PMC-w2v.bin PubMed-and-PMC-w2v_reduced.bin mrsty_vocab.csv # drop word embeddings out of the UMLS vocab 
 python calc_tsne.py PubMed-and-PMC-w2v_reduced.bin tsne_PubMed-and-PMC-w2v.csv # project the word embeddings down to 2 dimensions
 python calc_accuracy.py PubMed-and-PMC-w2v_reduced.bin analogies.txt accuracy_PubMed-and-PMC-w2v.csv 1 # evaluates the word embeddings based on an analogy task
